fa81f4d89b0cc9a83f60f63eb88c03f03e09b470
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-12-07 12:11:27 -0300

    import rpg-awesome

--- problema_6/src/main.tsx
+++ problema_6/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './assets/fonts.css'
+import './assets/rpg-awesome/css/rpg-awesome.min.css';
+import './assets/rpg-awesome/main.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
