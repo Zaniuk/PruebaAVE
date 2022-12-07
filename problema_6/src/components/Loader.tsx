a3492fc57fad5d609c7fba12494e44374637cde7
Zaniuk <61323637+Zaniuk@users.noreply.github.com>
2022-12-07 12:11:14 -0300

    implement loader

---
+++ problema_6/src/components/Loader.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
+function Loader() {
+  return (
+    <Box sx={{ 
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+
+     }}>
+      <CircularProgress />
+    </Box>
+  )
+}
+
+export default Loader
